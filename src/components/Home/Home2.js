23f9009fd1a28320d7fe0f4cc4a980cc4f418903
Joshua <146916593+joshhuu@users.noreply.github.com>
2025-04-23 14:08:03 +0530

    Update Home2.js

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -96,7 +96,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.instagram.com/_joshhuu_"
+                  href="https://www.instagram.com/sjoshua08"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour home-social-icons"
